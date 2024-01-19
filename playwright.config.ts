65599ff8ff591b51683b2acf3dcfb5cfb2a8ea8b
Shashank Sharma <shashanksharma.er104@gmail.com>
2024-01-19 16:26:17 +0530

    Added browser capability in playwright.congig.ts file

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
     testMatch: ["tests/recorded.test.ts"],
     use: {
+        browserName: "chromium",
         headless: false,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure'
