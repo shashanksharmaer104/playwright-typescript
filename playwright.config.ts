8ee1f640a7a0e48701a79dca910f594f0e9efcc7
Shashank Sharma <shashanksharma.er104@gmail.com>
2024-11-19 12:50:54 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ const config: PlaywrightTestConfig = {
     testMatch: ["pomtest/addtocart.test.ts"],
     use: {
         baseURL: "https://ecommerce-playground.lambdatest.io/index.php?",
-        browserName: "firefox",
+        browserName: "chromium",
         headless: true,
         launchOptions: {
             args: ['--start-maximized']
